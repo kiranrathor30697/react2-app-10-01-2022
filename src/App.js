2005d381afa70ca2e45d28ff4cd19322213a8c23
kiranrathor30697 <kiranrathor30697@gmail.com>
2022-01-11 17:05:57 -1100

    v1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <div className='container'>
+        <ul className="nav nav-tabs outline">
+           
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Dropdown</a>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Action</a></li>
+                <li><a className="dropdown-item" href="#">Another action</a></li>
+                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                <li><a className="dropdown-item" href="#">Separated link</a></li>
+              </ul>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">All</a>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Action</a></li>
+                <li><a className="dropdown-item" href="#">Another action</a></li>
+                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                <li><a className="dropdown-item" href="#">Separated link</a></li>
+              </ul>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Dropdown</a>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Action</a></li>
+                <li><a className="dropdown-item" href="#">Another action</a></li>
+                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                <li><a className="dropdown-item" href="#">Separated link</a></li>
+              </ul>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Login</a>
+              <ul className="dropdown-menu">
+                <li><a className="dropdown-item" href="#">Action</a></li>
+                <li><a className="dropdown-item" href="#">Another action</a></li>
+                <li><a className="dropdown-item" href="#">Something else here</a></li>
+                <li><a className="dropdown-item" href="#">Separated link</a></li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+          
       </header>
-    </div>
+      <main><h1>Main</h1></main>
+      <footer><h1>Footer</h1></footer>
+    </>
   );
 }
 
---
+++ src/Box.js
@@ -0,0 +1,32 @@
+import './App.css';
+
+export default function Box() {
+    return (
+        <div className='box1'>
+            <div className='box1_1 common rounded-circle hidden'>
+                <div className='box2_1 common1'></div>
+                <div className='box2_2 common1'></div>
+                <div className='box2_3 common1'></div>
+                <div className='box2_4 common1'></div>
+            </div>
+            <div className='box1_2 common rounded-circle hidden'>
+                <div className='box3_1 common1'></div>
+                <div className='box3_2 common1'></div>
+                <div className='box3_3 common1'></div>
+                <div className='box3_4 common1'></div>
+            </div>
+            <div className='box1_3 common rounded-circle hidden'>
+                <div className='box4_1 common1'></div>
+                <div className='box4_2 common1'></div>
+                <div className='box4_3 common1'></div>
+                <div className='box4_4 common1'></div>
+            </div>
+            <div className='box1_4 common rounded-circle hidden'>
+                <div className='box5_1 common1'></div>
+                <div className='box5_2 common1'></div>
+                <div className='box5_3 common1'></div>
+                <div className='box5_4 common1'></div>
+            </div>
+        </div>
+    )
+}
